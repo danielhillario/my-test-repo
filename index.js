47716782b28922cb1d9100907942b9a7cc3d9f55
danielhillario <danielhillario@gmail.com>
2021-08-24 14:53:55 +0800

    Finish working with API

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+// We want to talk to an API:
+// What is out endpoint
+let url = `https://restcountries.eu/rest/v2/name/Malaysia`;
+
+function waitCountryData(response) {
+    return response.json();
+}
+
+function countriesData(data) {
+    let pop = data[0].population;
+    console.log(`The population of Malaysia is ${pop}`);
+    debugger;
+}
+
+
+fetch(url, { method: "GET" }).then(waitCountryData).then(countriesData);
