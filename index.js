ad0b46173085bd581bb9a3ee6cac105e79e0b0ff
danielhillario <danielhillario@gmail.com>
2021-08-24 14:14:42 +0800

    Added a div tag in index.js and console.log in index.js

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-// Working with APIs
-// 
-// We need to answer the following questions
-// 
-// -What is the endpoint (URL)?
-// -What query string/parametes do we need to provide
-// - What are required parameters
-// - What optional parameters do we want to add
-// - What HTTP method are we using?
+console.log("Hello World");
